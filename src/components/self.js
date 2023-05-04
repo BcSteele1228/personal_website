90814642cc9367a4a260726afe43b8fc42f1b565
Brady Steele <bcsteele1228@gmail.com>
2023-05-04 18:34:22 -0500

    Added animations for logos, and react-type-animation for faux typing about me section

--- src/components/self.js
+++ src/components/self.js
@@ -35,15 +35,6 @@ const Self = () => {
     config: { duration: 2000 }
   });
 
-  const intro =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const introTrail = useTrail(intro.length, {
-    to: { opacity: 1, transform: "translateY(0)" },
-    from: { opacity: 0, transform: "translateY(0)" },
-    delay: 0,
-    config: { duration: 65 }
-  });
-
   return (
     <section className="px-5 py-20 lg:py-32 bg-gray-200 text-gray-900">
       <div className="container mx-auto lg:flex items-center justify-between">
@@ -59,16 +50,16 @@ const Self = () => {
               <AiOutlineGithub size={48} />
             </a>
           </animated.div>
-          <animated.div style={seeProjects} classname="flex mb0-">
-          <a
-            href="/#projects"
-            className="btn bg-gray-700 text-white px-6 py-3 hover:bg-white hover:text-gray-700 border-2 border-gray-700"
-          >
-            See Projects
-          </a>
+          <animated.div style={seeProjects} className="flex mb0-">
+            <a
+              href="/#projects"
+              className="btn bg-gray-700 text-white px-6 py-3 hover:bg-white hover:text-gray-700 border-2 border-gray-700"
+            >
+              See Projects
+            </a>
           </animated.div>
         </animated.div>
-
+  
         <div className="lg:w-1/2">
           <h2 className="text-3xl lg:text-6xl font-bold leading-tight mb-6">
             {aboutTrail.map((props, index) => (
@@ -77,18 +68,23 @@ const Self = () => {
               </animated.span>
             ))}
           </h2>
-
-          <p className="text-lg md:text-xl mb-8">
-            {introTrail.map((props, index) => (
-              <animated.span key={index} style={props}>
-                {intro[index]}
-              </animated.span>
-            ))}
-          </p>
+              <TypeAnimation
+                className="font-normal text-xl md:text-lg mb-8"
+                cursor={true}
+                wrapper="div"
+                style={{ fontSize: '1.5em', display: 'inline-block' }}
+                repeat={1}
+                speed={60}
+                sequence={[
+                  1000,
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                ]}
+              />
         </div>
       </div>
     </section>
   );
+  
 };
 
 export default Self;
