876f33167a94fb109d1331730c9603e7778c5bd1
Brady Steele <bcsteele1228@gmail.com>
2023-05-19 15:37:35 -0500

    Added floating animation to characterImage and resized on loadingPage with new image

--- src/components/loadingPage.js
+++ src/components/loadingPage.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import * as THREE from 'three';
 import { TextureLoader } from 'three';
-
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 
 import characterImage from '../assets/astronaut.png';
 
-function Character({ position, speed }) {
+function Character({ speed }) {
+  const meshRef = useRef();
   const [upward, setUpward] = useState(true);
+  const [position, setPosition] = useState(0);
 
-  useFrame(() => {
-    // Move the character up and down
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const mesh = meshRef.current;
+
     if (upward) {
-      position.y += speed;
-      if (position.y > 1) {
+      setPosition((prevPosition) => prevPosition + speed);
+      mesh.position.y = position;
+      if (position >= 0.5) {
         setUpward(false);
       }
     } else {
-      position.y -= speed;
-      if (position.y < -1) {
+      setPosition((prevPosition) => prevPosition - speed);
+      mesh.position.y = position;
+      if (position <= -0.5) {
         setUpward(true);
       }
     }
@@ -29,10 +33,12 @@ function Character({ position, speed }) {
   const texture = useLoader(TextureLoader, characterImage);
 
   return (
-    <mesh position={position}>
-      <planeBufferGeometry args={[9, 9]} />
-      <meshBasicMaterial map={texture} transparent />
-    </mesh>
+    <group>
+      <mesh ref={meshRef}>
+        <boxBufferGeometry args={[5, 5, 0.1]} />
+        <meshBasicMaterial map={texture} transparent />
+      </mesh>
+    </group>
   );
 }
 
@@ -130,33 +136,40 @@ function LoadingPage() {
         value: 5,
       },
     },
-  };  
+  };
+
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen">
-      <div className="particles-container">
+      <div className="particles-container" style={{ display: particlesLoaded ? 'block' : 'none' }}>
         <Particles options={particleOptions} init={particlesInit} loaded={onParticlesLoaded} />
       </div>
-      <div className="relative z-10" style={{ display: particlesLoaded ? 'flex' : 'none' }}>
-        {loading ? (
-          <div className="flex flex-col justify-center items-center w-full h-full">
-            <Canvas className="w-full h-full">
-              <ambientLight />
-              <pointLight position={[10, 10, 10]} />
-              <Character position={[0, 0, 0]} speed={0.02} />
-            </Canvas>
-            <p className="text-lg mt-4" style={{ color: 'white' }}>
-              Loading Brady's Portfolio...
-            </p>
-            <div className="w-1/2 h-2 bg-gray-200 mt-4 rounded-full">
-              <div className="h-2 bg-pink-400 rounded-full" style={{ width: `${progress * 100}%` }}></div>
-            </div>
-            <div className="text-white mt-2">{Math.round(progress * 100)}%</div>
-          </div>
-        ) : (
+      {loading ? (
+        <div className="relative z-10 flex flex-col justify-center items-center w-full h-full">
           <Canvas className="w-full h-full">
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <group>
+              <mesh className="animate-float motion-safe">
+                <Character speed={0.01} />
+              </mesh>
+            </group>
           </Canvas>
-        )}
-      </div>
+          <p className="text-lg mt-4" style={{ color: 'white' }}>
+            Loading Brady's Portfolio...
+          </p>
+          <div className="w-1/4 h-2 bg-gray-200 mt-4 rounded-full">
+            <div className="h-2 bg-pink-400 rounded-full" style={{ width: `${progress * 100}%` }}></div>
+          </div>
+          <div className="text-white mt-2">{Math.round(progress * 100)}%</div>
+        </div>
+      ) : (
+        <div className="w-full h-full" style={{ position: 'absolute' }}>
+          <Canvas className="w-full h-full">
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+          </Canvas>
+        </div>
+      )}
     </div>
   );
 }
